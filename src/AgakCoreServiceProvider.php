2f0f88126029d3067de7d5e34c35fa644e6db9cc
mhafizhasan <mhafizhasan@gmail.com>
2016-12-13 17:13:57 +0800

    add migrations tag

--- src/AgakCoreServiceProvider.php
+++ src/AgakCoreServiceProvider.php
@@ -14,7 +14,7 @@ class AgakCoreServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/migrations/create_activity_log_table.php' => base_path('database/migrations/create_activity_log_table.php')
+            __DIR__.'/../migrations/create_activity_log_table.php' => base_path('database/migrations/create_activity_log_table.php')
         ],'migrations');
     }
 
