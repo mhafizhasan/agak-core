608187b9219657d88399197f87be7e39795c5a4a
mhafizhasan <mhafizhasan@gmail.com>
2016-12-13 18:31:43 +0800

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # AGAK CORE
 
-A core package for AGAK. List of features:  
+A core package for AGAK Platform.
+
+List of features:  
 1. Activity logger  
 2. API
 
--- src/AgakCoreServiceProvider.php
+++ src/AgakCoreServiceProvider.php
@@ -13,7 +13,6 @@ class AgakCoreServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // publish on boot
         $this->publishes([
             __DIR__.'/../migrations/create_activity_log_table.php' => database_path('/migrations/create_activity_log_table.php')
         ],'migrations');
