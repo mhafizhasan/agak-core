7304676e10c991de99bb61a67eaf77de4da26692
mhafizhasan <mhafizhasan@gmail.com>
2017-01-04 10:27:47 +0800

    update AgakGateKeeper

--- src/Middleware/AgakGateKeeper.php
+++ src/Middleware/AgakGateKeeper.php
@@ -40,10 +40,11 @@ class AgakGateKeeper extends BaseMiddleware
         $permitted_path = array(
             "dashboard",
             "profile",
-            "module"
+            "module",
+            "user"
         );
 
-        if($user->hasRole('developer')) {
+        if($user->hasRole(['developer','sysadmin'])) {
 
             $developer_path = array(
                 "developer"
@@ -52,7 +53,7 @@ class AgakGateKeeper extends BaseMiddleware
             $permitted_path = array_merge($permitted_path, $developer_path);
         }
 
-        if($user->hasRole('admin') || $user->hasRole('sysadmin')) {
+        if($user->hasRole(['admin','sysadmin'])) {
 
             $admin_path = array(
                 "admin",
@@ -71,6 +72,7 @@ class AgakGateKeeper extends BaseMiddleware
             $permitted_path = array_merge($permitted_path, $sysadmin_path);
         }
 
+        // Check subscribed modules
         if(!in_array($url, $permitted_path)) {
 
             $upper_tree = AgakAPI::parentNode($user->org_code, false);
@@ -111,76 +113,6 @@ class AgakGateKeeper extends BaseMiddleware
             }
 
         }
-
-
-
-
-
-
-        // $uid = Session::get('uid');
-        // $org_code = Session::get('org_code');
-        //
-        // $user = User::where('uid', '=', $uid)->first();
-        //
-        // $url = \Request::path();
-        //
-        // $permitted_path = array(
-        //     "dashboard",
-        //     "profile",
-        //     "module"
-        // );
-        //
-        // if($user->hasRole('developer')) {
-        //
-        //     $developer_path = array(
-        //         "developer"
-        //     );
-        //
-        //     $permitted_path = array_merge($permitted_path, $developer_path);
-        // }
-        //
-        // if($user->hasRole('admin') || $user->hasRole('sysadmin')) {
-        //
-        //     $admin_path = array(
-        //         "admin"
-        //     );
-        //
-        //     $permitted_path = array_merge($permitted_path, $admin_path);
-        // }
-        //
-        // if(!in_array($url, $permitted_path)) {
-        //
-        //     // Check user access for module
-        //     $orgs = AgakAPI::parentNode($org_code, false);
-        //
-        //     $my_orgs = array();
-        //     foreach($orgs as $val) {
-        //         array_push($my_orgs, $val->code);
-        //     }
-        //     // Add current user node to array
-        //     array_push($my_orgs, $org_code);
-        //     $valid_org = implode(',', $my_orgs);
-        //
-        //     // DB::setFetchMode(\PDO::FETCH_ASSOC);
-        //     $valid_path = DB::table('module_subscriber')
-        //                 ->join('modules', 'modules.code', '=', 'module_subscriber.module_code')
-        //                 ->select('modules.url')
-        //                 ->whereIn('organisation_code', [$valid_org])->get();
-        //
-        //     print_r($valid_path);
-        //
-        //     $ok = 0;
-        //     foreach($valid_path as $vp) {
-        //         if($url === $vp->url)
-        //             $ok = 1;
-        //     }
-        //
-        //     if($ok != 1) {
-        //         abort(400, 'Unauthorized area.');
-        //     }
-        //
-        // }
-
 
         return $next($request);
     }
